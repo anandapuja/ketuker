172169deaf7d6ff57842c1789d8ef857c9a1ebc0
anandapuja <anandapujawandra@gmail.com>
2020-05-29 14:54:42 +0700

    login register routing

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default function Register(){
+export default function Register () {
   return (
-    <></>
-  )
+    <>
+      Register
+    </>
+  );
 }
